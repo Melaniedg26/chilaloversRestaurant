d3591b7f829d5c30bcfb54abfd8c9102814f08f9
Melaniedg26 <melanievm8@gmail.com>
2024-03-02 00:41:50 -0700

    Barra de navegacion Inferior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { TabsComponent } from './core/components/tabs/tabs.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule,RouterOutlet,RouterModule,TabsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
