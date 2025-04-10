3c8eda45df405a0578b8e0d81c01553722bcbb5e
Melaniedg26 <melanievm8@gmail.com>
2025-04-10 12:29:09 -0700

    Calcula deliverys

--- src/app/pages/carrito/carrito.component.ts
+++ src/app/pages/carrito/carrito.component.ts
@@ -6,11 +6,12 @@ import { ContadorCantidadComponent } from "../../core/components/contador-cantid
 import { HttpClientModule } from '@angular/common/http';
 import { Producto } from '../../core/interfaces/productos';
 import { ProductosService } from '../../core/services/productos.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-carrito',
   standalone: true,
-  imports: [CommonModule, ContadorCantidadComponent, HttpClientModule],
+  imports: [CommonModule, ContadorCantidadComponent, HttpClientModule, RouterModule],
   templateUrl: './carrito.component.html',
   styleUrl: './carrito.component.scss'
 })
@@ -22,15 +23,33 @@ export class CarritoComponent {
 
   productosCarrito: Producto[] = [];
 
+subtotal=0;
+delivery=30;
+total=0;
+
   ngOnInit(): void {
     this.headerService.titulo.set("Carrito");
     this.cartService.carrito.forEach(async itemCarrito => {
       const res = await this.productosServide.getById(itemCarrito.idProducto)
       if (res) this.productosCarrito.push(res);
+      this.calcularInformacion();
     })
   }
 
   eliminarProducto(idProducto: number) {
     this.cartService.eliminarProducto(idProducto);
+  }
+
+  calcularInformacion(){
+    this.subtotal=0;
+  for (let i = 0; i < this.cartService.carrito.length; i++) {
+    this.subtotal+=this.productosCarrito[i].precio * this.cartService.carrito[i].cantidad;
+  }
+  this.total=this.subtotal+this.delivery;
+  }
+
+  cambiarCantidadProducto(id:number, cantidad:number){
+    this.cartService.cambiarCantidadProducto(id,cantidad);
+    this.calcularInformacion();
   }
 }
